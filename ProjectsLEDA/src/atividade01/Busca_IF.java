e84ae01b93c5741bab2fac4bf5ce8dff2e06ce2c
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-08-28 01:21:45 -0300

    criacao classe filme e interface busca

---
+++ ProjectsLEDA/src/atividade01/Busca_IF.java
@@ -0,0 +1,10 @@
+package atividade01;
+
+public interface Busca_IF {
+	//TODO exceptions here
+	Filme buscaLinearIterativa(Filme[] filmes, int nota);
+	Filme buscaLinearRecursiva(Filme[] filmes, int nota);
+	Filme buscaBinariaIterativa(Filme[] filmes, int nota);
+	Filme buscaBinariaRecursiva(Filme[] filmes, int nota);
+	Filme buscaLinearIterativaDuasPontas(Filme[] filmes, int nota);
+}
--- ProjectsLEDA/src/atividade01/Filme.java
+++ ProjectsLEDA/src/atividade01/Filme.java
@@ -1,5 +1,55 @@
 package atividade01;
 
-public class Filme {
+public class Filme implements Comparable<Filme>{
+	
+	private String nome;
+	private int nota;
+	private int ano;
+	
+	Filme(String nome, int nota, int ano){
+		this.nome = nome;
+		this.nota = nota;
+		this.ano = ano;
+	}
+	
+	@Override
+	public int compareTo(Filme o) {
+		// TODO 
+		return 0;
+	}
 
+	
+	@Override
+	public String toString() {
+		// TODO
+		return "Filme [nome=" + nome + ", nota=" + nota + ", ano=" + ano + "]";
+	}
+
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	
 }
