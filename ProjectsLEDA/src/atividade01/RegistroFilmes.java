a201d70798e997a6912c7c72a4b81d4a6d8e63b9
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-08-30 14:16:56 -0300

    lista

--- ProjectsLEDA/src/atividade01/RegistroFilmes.java
+++ ProjectsLEDA/src/atividade01/RegistroFilmes.java
@@ -9,21 +9,21 @@ public class RegistroFilmes {
 	public static void main(String[] args) {
 		
 		//apenas para testar
-		Filme f1 = new Filme("Caroline", 3, 2004);
-		Filme f2 = new Filme("Nemo", 4, 1944);
-		Filme f3 = new Filme("Lisbela", 5, 2004);
-		Filme f4 = new Filme("Divertidamente", 5, 2004);
-		Filme f5 = new Filme("A que pena seria", 4, 2008);
-		Filme f6 = new Filme("A pequena sereia", 3, 2000);
-		Filme f7 = new Filme("O menino do pijama listrado", 2, 2015);
-		Filme f8 = new Filme("O pijama do menino listrado", 1, 2000);
-		Filme f9 = new Filme("Tarzan", 5, 1994);
-		Filme f10 = new Filme("Zootopia", 5, 2017);
+		Filme f1 = new Filme("Caroline", 2004, 3);
+		Filme f2 = new Filme("Nemo", 1944, 4);
+		Filme f3 = new Filme("Lisbela", 2004, 5);
+		Filme f4 = new Filme("Divertidamente", 2004, 5);
+		Filme f5 = new Filme("A que pena seria", 2008, 4);
+		Filme f6 = new Filme("A pequena sereia", 2000, 3);
+		Filme f7 = new Filme("O menino do pijama listrado", 2015, 2);
+		Filme f8 = new Filme("O pijama do menino listrado", 2000, 1);
+		Filme f9 = new Filme("Tarzan", 1994, 5);
+		Filme f10 = new Filme("Zootopia", 2017, 5);
 		
 		Filme[] filmes = {f1, f2, f3, f4, f5, f6, f7, f8, f9, f10};
 		
 		
-	//rdenacao ordenar = new Ordenacao();
+		
 		//for (Filme f : filmes) System.out.print(f);
 		/*
 		ordenar.insertionSort(filmes);
@@ -33,8 +33,19 @@ public class RegistroFilmes {
 		//System.out.println(lista);
 		
 		
-		Arrays.sort(filmes);
-		for (Filme f : filmes) System.out.print(f);
+		
+		for (Filme f : filmes) {
+			System.out.println(f);
+		}
+		
+		
+		System.out.println("Lista Ordenada:");
+		Ordenacao.quickSort(filmes, 0, filmes.length-1);
+		
+		for (Filme f : filmes) {
+			System.out.println(f);
+		}
+		
 		Busca_IF busca = new Busca();
 		Filme filmeEncontrado;
 		
