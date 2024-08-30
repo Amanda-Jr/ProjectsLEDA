5960d5e574f89e49b801875c4bfc65375a40dfb9
William Pereira <almeidapereirast@gmail.com>
2024-08-30 13:26:04 -0300

    Update Filme.java
    compareTo - nota valor decrescente

--- ProjectsLEDA/src/atividade01/Filme.java
+++ ProjectsLEDA/src/atividade01/Filme.java
@@ -1,74 +1,48 @@
 package atividade01;
 
 public class Filme implements Comparable<Filme>{
-	
 	private String nome;
-	private int nota;
-	private int ano;
+	private int ano, nota;
 	
-	Filme(String nome, int nota, int ano){
+	Filme(String nome, int ano, int nota){
 		this.nome = nome;
-		this.nota = nota;
 		this.ano = ano;
+		this.nota = nota;
 	}
-	
-	@Override
-	//TODO - not finished
-	public int compareTo(Filme outro) {
-		if(this.nota != outro.nota) {
-			if(this.nota < outro.nota) {
-				return 1;
-			}
-			else if(this.nota > outro.nota){
-				return -1;
-			}
-		} 
-		else if (this.ano != outro.ano) {
-			if(this.ano < outro.ano) {
-				return -1;
-			}
-			else if(this.ano > outro.ano){
-				return 1;
-			}
-		} 
-		else if (!this.nome.equals(outro.nome)){
-			return this.nome.compareTo(outro.nome);
-		}
-			
-		return 0;
-	}
-
 	
 	@Override
 	public String toString() {
-		// TODO
-		return "Filme [nome = " + nome + ", nota = " + nota + ", ano = " + ano + "]\n";
+		return "Filme [nome=" + nome + ", ano=" + ano + ", nota=" + nota + "]";
 	}
 
-	
+	@Override
+    public int compareTo(Filme outro) {
+    	if (this.nota != outro.nota) {
+    		return -1 * Integer.compare(this.nota, outro.nota);
+    	} else if (this.ano != outro.ano) {
+    		return Integer.compare(this.ano, outro.ano);
+    	}   else {
+    		return this.nome.compareTo(outro.nome);
+    	}
+	}
 	
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public int getNota() {
-		return nota;
-	}
-
-	public void setNota(int nota) {
-		this.nota = nota;
-	}
-
 	public int getAno() {
 		return ano;
 	}
-
 	public void setAno(int ano) {
 		this.ano = ano;
+	}
+	public int getNota() {
+		return nota;
+	}
+	public void setNota(int nota) {
+		this.nota = nota;
 	}
 	
 	
