b27d1428d02ae7bddec1cc20fd83d72b41a68077
William Pereira <almeidapereirast@gmail.com>
2024-08-30 13:27:50 -0300

    Update Ordenacao.java
    funcoes Swap, Partition, e QuickSort

--- ProjectsLEDA/src/atividade01/Ordenacao.java
+++ ProjectsLEDA/src/atividade01/Ordenacao.java
@@ -20,9 +20,37 @@ public class Ordenacao {
 	public void mergeSort(Filme[] filmes) {
 		
 	}
-	
 
 	
+	public static void swap(Filme A[], int i, int j) {
+		Filme temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+	}
 	
+	public static int partition(Filme A[], int left, int right) {
+		Filme p = A[left];
+		int i = left + 1;
+		int j = right;
+		
+		while(i <= j) {
+			if(A[i].compareTo(p) <= 0) {
+				i++;
+			}else if(A[j].compareTo(p) > 0) {
+				j--;
+			}else {
+				swap(A, i, j);
+			}
+		}
+		swap(A, left, j);
+		return j;
+	}
 	
+	public static void quickSort(Filme A[], int left, int right) {
+		if(left < right) {
+			int pivot = partition(A, left, right);
+			quickSort(A, left, pivot-1);
+			quickSort(A, pivot+1, right);
+		}
+	}
 }
