c14eabad096c5841a87c30687dd2e3180e1b5bce
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-09-04 20:56:01 -0300

    atualizacao

--- ProjectsLEDA/src/atividade01/RegistroFilmes.java
+++ ProjectsLEDA/src/atividade01/RegistroFilmes.java
@@ -10,16 +10,16 @@ public class RegistroFilmes {
 		
 		//apenas para testar
 		System.out.println("\nLista Original:");
-		Filme f1 = new Filme("Caroline", 2004, 3);
-		Filme f2 = new Filme("Nemo", 1994, 4);
-		Filme f3 = new Filme("Lisbela", 2004, 5);
-		Filme f4 = new Filme("Divertidamente", 2004, 5);
-		Filme f5 = new Filme("A que pena seria", 2008, 4);
-		Filme f6 = new Filme("A pequena sereia", 2000, 3);
-		Filme f7 = new Filme("O menino do pijama listrado", 2015, 2);
-		Filme f8 = new Filme("O pijama do menino listrado", 2000, 2);
-		Filme f9 = new Filme("Tarzan", 1994, 4);
-		Filme f10 = new Filme("Zootopia", 2017, 5);
+		Filme f1 = new Filme("Caroline", 3, 2004);
+		Filme f2 = new Filme("Nemo", 4, 1994 );
+		Filme f3 = new Filme("Lisbela", 5, 2004);
+		Filme f4 = new Filme("Divertidamente", 5, 2004);
+		Filme f5 = new Filme("A que pena seria", 4, 2008);
+		Filme f6 = new Filme("A pequena sereia", 3, 2000);
+		Filme f7 = new Filme("O menino do pijama listrado", 2, 2015);
+		Filme f8 = new Filme("O pijama do menino listrado", 2, 2000);
+		Filme f9 = new Filme("Tarzan", 4, 1994);
+		Filme f10 = new Filme("Zootopia", 5, 2017);
 		
 		Filme[] filmes = {f1, f2, f3, f4, f5, f6, f7, f8, f9, f10};
 		
@@ -33,13 +33,13 @@ public class RegistroFilmes {
 		//ordenacao.quickSort(filmes.clone());
 		//ordenacao.quickSortRandom(filmes.clone());
 		//ordenacao.insertionSort(filmes.clone());
-		ordenacao.mergeSort(filmes.clone());
+		ordenacao.mergeSort(filmes);
 		//ordenacao.countingSort(filmes.clone());
 		
 		Busca_IF busca = new Busca();
 		Filme filmeEncontrado;
 		
-		if(ordenacao.checaVetorOrdenado(filmes)) {
+		if(ordenacao.checaVetorOrdenado(filmes.clone())) {
 			System.out.println("\nLista Ordenada:");
 			
 			for (Filme f : filmes) {
@@ -48,11 +48,11 @@ public class RegistroFilmes {
 			
 			try {
 					
-				//filmeEncontrado = busca.buscaLinear_iterativa(filmes.clone(), 4);
-				//filmeEncontrado = busca.buscaLinear_recursiva(filmes.clone(), 2);
-				//filmeEncontrado = busca.buscaBinaria_iterativa(filmes.clone(), -1);
-				//filmeEncontrado = busca.buscaBinaria_recursiva(filmes.clone(), 1);
-				filmeEncontrado = busca.buscaLinear_iterativa_duasPontas(filmes, 2);
+				filmeEncontrado = busca.buscaLinear_iterativa(filmes, 4);
+				//filmeEncontrado = busca.buscaLinear_recursiva(filmes, 2);
+				//filmeEncontrado = busca.buscaBinaria_iterativa(filmes, -1);
+				//filmeEncontrado = busca.buscaBinaria_recursiva(filmes, 1);
+				//filmeEncontrado = busca.buscaLinear_iterativa_duasPontas(filmes, 2);
 					
 				if(filmeEncontrado != null) {
 					System.out.println("\nFilme encontrado: " + filmeEncontrado);
