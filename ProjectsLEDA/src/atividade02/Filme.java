4c2f2ba4c4f01ec9b7793a6ecc16129a7e88343c
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-10-10 14:31:27 -0300

    compareTo

--- ProjectsLEDA/src/atividade02/Filme.java
+++ ProjectsLEDA/src/atividade02/Filme.java
@@ -5,6 +5,44 @@ public class Filme implements Filme_IF{
 	private int ano, nota;
 	private long ID;
 	
+	Filme(String nome, int ano, int nota, long ID){
+		this.nome = nome;
+		this.ano = ano;
+		this.nota = nota;
+		this.ID = ID;
+	}
+	
+	@Override
+	public int compareTo(Filme_IF outro_filme) {
+		if(this.ID != outro_filme.getID()) {
+			return Long.compare(this.ID, outro_filme.getID());
+		}
+		else if (this.nota != outro_filme.getNota()) {
+    		return -1 * Integer.compare(this.nota, outro_filme.getNota());
+    	} 
+		else if (this.ano != outro_filme.getAno()) {
+    		return Integer.compare(this.ano, outro_filme.getAno());
+    	} 
+		else {
+    		return this.nome.compareTo(outro_filme.getNome());
+    	}
+		
+	}
+	
+	public int compareToNota(Filme_IF outro_filme) {
+    	if (this.nota != outro_filme.getNota()) {
+    		return -1 * Integer.compare(this.nota, outro_filme.getNota());
+    	} 
+    	return 0;
+	}
+	
+	public int compareToID(Filme_IF outro_filme) {
+		if(this.ID != outro_filme.getID()) {
+			return Long.compare(this.ID, outro_filme.getID());
+		} 
+    	return 0;
+	}
+	
 	@Override
 	public long getID() {
 		// TODO Auto-generated method stub
@@ -45,9 +83,5 @@ public class Filme implements Filme_IF{
 		// TODO Auto-generated method stub
 		
 	}
-	@Override
-	public int compareTo(Filme_IF outro_filme) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	
 }
