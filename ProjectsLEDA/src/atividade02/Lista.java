302db651b0256d2d64dcb69f4489cf76a63ee9f8
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-10-12 23:54:14 -0300

    No Lista gets and sets

--- ProjectsLEDA/src/atividade02/Lista.java
+++ ProjectsLEDA/src/atividade02/Lista.java
@@ -1,6 +1,8 @@
 package atividade02;
 
 public class Lista implements Lista_IF{
+	
+	
 
 	@Override
 	public Filme_IF remove(long id) throws Exception {
@@ -10,37 +12,37 @@ public class Lista implements Lista_IF{
 
 	@Override
 	public void insert(Filme_IF elemento) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
 	@Override
 	public Filme_IF search(long id) throws Exception {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Filme_IF head() throws Exception {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public Filme_IF tail() throws Exception {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
--- ProjectsLEDA/src/atividade02/NoLista.java
+++ ProjectsLEDA/src/atividade02/NoLista.java
@@ -2,9 +2,9 @@ package atividade02;
 
 public class NoLista {
 	
-	public Filme valor;
-	public NoLista prox;
-	public NoLista ant;
+	private Filme valor;
+	private NoLista prox;
+	private NoLista ant;
 	
 	public boolean isNil() {
 		if(valor == null) {
@@ -13,4 +13,30 @@ public class NoLista {
 		
 		return false;
 	}
+
+	public Filme getValor() {
+		return valor;
+	}
+
+	public void setValor(Filme valor) {
+		this.valor = valor;
+	}
+
+	public NoLista getProx() {
+		return prox;
+	}
+
+	public void setProx(NoLista prox) {
+		this.prox = prox;
+	}
+
+	public NoLista getAnt() {
+		return ant;
+	}
+
+	public void setAnt(NoLista ant) {
+		this.ant = ant;
+	}
+	
+	
 }
