0a6f23f97f275f2f6af2274f77e51cf4aeb5ba74
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-10-19 21:03:43 -0300

    Fila

--- ProjectsLEDA/src/atividade02/Fila.java
+++ ProjectsLEDA/src/atividade02/Fila.java
@@ -1,29 +1,37 @@
 package atividade02;
 
 public class Fila implements Fila_IF{
+	
+	Pilha p1 = new Pilha();
+	Pilha p2 = new Pilha();
+	
+	
+	@Override 
+	public void enqueue(Filme_IF elemento) {
+		p1.push(elemento);
+	}
 
 	@Override
 	public Filme_IF dequeue() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		while(!p1.isEmpty()) {
+			p2.push(p1.top());
+		}
+		Filme_IF removido = p2.pop();
+		while(!p2.isEmpty()) {
+			p1.push(p2.top());
+		}
+		return removido;
 	}
 
-	@Override
-	public void enqueue(Filme_IF elemento) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return p1.isEmpty();
 	}
 
 	@Override
 	public Filme_IF head() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return p1.top();
 	}
 
 }
