3894d90778affb0c3317643370f35a3c51ea6859
weep_dev <almeidapereirast@gmail.com>
2024-10-31 19:08:28 -0300

    arrumada o ngc da arvore

--- ProjectsLEDA/src/atividade02/Main.java
+++ ProjectsLEDA/src/atividade02/Main.java
@@ -4,13 +4,20 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		
-		Filme f1 = new Filme("Tarzan", 2004, 5, 3);
-		Filme f2 = new Filme("Tom e Jerry", 2022, 2, 55);
-		Filme f3 = new Filme("Ben 10", 2001, 1, 26);
-		Filme f4 = new Filme("Transformers", 2007, 5, 13);
-		Filme f5 = new Filme("McQueen", 2022, 2, 12);
-		Filme f6 = new Filme("SQL O Filme", 2001, 1, 39);
+		Filme_IF[] filmes = {
+				new Filme("A", 5, 2020, 1),
+				new Filme("B", 5, 2020, 3),
+			    new Filme("C", 4, 2019, 5),
+			    new Filme("D", 4, 2000, 8),
+			    new Filme("E", 3, 2005, 2),
+			    new Filme("F", 3, 2002, 6),
+			    new Filme("G", 3, 2005, 4),
+			    new Filme("H", 2, 2019, 7),
+			    new Filme("I", 2, 2020, 9),
+			    new Filme("J", 1, 1998, 10)
+			};
 		
+		/*
 		TabelaHash t = new TabelaHash();
 		Lista l = new Lista();
 		Pilha p = new Pilha();
@@ -41,7 +48,7 @@ public class Main {
 		f.enqueue(f3);
 		
 		
-		/*
+		
 		System.out.println(l.size());
 		l.print(l.getFim());
 		System.out.println("");
@@ -51,8 +58,6 @@ public class Main {
 		System.out.println("\nlista ordenada");
 		l = l.ordernar();
 		l.print(l.getFim());
-		*/
-		
 		
 		System.out.println(t.isEmpty());
 		
@@ -65,10 +70,30 @@ public class Main {
 		System.out.println("Tabela Hash Ordenada: ");
 		t.ordenar();
 		System.out.println(t.print());
+		*/
 		
 		//System.out.println(t.search(55));
 		//System.out.println(t.search(3));
 		//System.out.println(t.search(45));
+		
+		AVL avl = new AVL();
+		System.out.println(avl.isEmpty());
+		
+		avl.insert(filmes[0]); // id = 1
+		avl.insert(filmes[1]); // id = 3
+		avl.insert(filmes[2]); // id = 5
+		avl.insert(filmes[3]); // id = 8
+		avl.insert(filmes[4]); // id = 2
+		avl.insert(filmes[5]); // id = 6
+		avl.insert(filmes[6]); // id = 4
+		avl.insert(filmes[7]); // id = 7
+		avl.insert(filmes[8]); // id = 9
+		avl.insert(filmes[9]); // id = 10
+		
+		System.out.println(avl.isEmpty());
+		
+		Filme_IF filme = avl.remove(3);
+		System.out.println(filme);
 	}
 
 }
